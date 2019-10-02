d471205d5701e9f7cf0c65153d869603711d7d51
Josh Natis <joshnatis0@gmail.com>
2019-10-02 19:55:53 -0400

    Upload metroloop.mp3

--- tunes/js/index.js
+++ tunes/js/index.js
@@ -407,6 +407,11 @@ jQuery(function ($) {
             	"name": "library holly",
             	"duration": "1:23",
             	"file": "https://github.com/joshnatis/josh8/blob/master/tunes/songs/__library_holly.mp3?raw=true"
+            }, {
+            	"track": 78,
+            	"name": "library holly",
+            	"duration": "1:23",
+            	"file": "https://github.com/joshnatis/josh8/blob/master/tunes/songs/__metroloop.mp3?raw=true"
             }],
             
             buildPlaylist = $(tracks).each(function(key, value) {
