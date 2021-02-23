6375e749c15922c868b9abb1bddf1539c77eb38e
joshnatis <joshnatis0@gmail.com>
2021-02-23 00:51:10 -0500

    Finish fuzzy search functionality

--- 404/404.js
+++ 404/404.js
@@ -13,6 +13,30 @@ function randomizeTexture()
 	document.body.style.backgroundImage = "url('404/images/texture" + n + ".png')";
 }
 
+function hammingDistance(target, actual)
+{
+	let max = (target.length > actual.length) ? target.length : actual.length;
+	let min = (target.length > actual.length) ? actual.length : target.length;
+	let distance = max - min;
+
+	for(let i = 0; i < target.length; ++i)
+		if(target[i] != actual[i])
+			distance++;
+
+	return distance;
+}
+
+function fuzzySearch(target, table)
+{
+	const THRESHOLD = 5; /* lenience of fuzzy search */
+	for(let item in table)
+	{
+		if(hammingDistance(target, item) <= THRESHOLD)
+			return [true, key];
+	}
+	return [false, ""];
+}
+
 function displayFuzzySearch()
 {
 	const PAGES = ["cybernetic98", "papers", "whom", "eng2ru", "professors",
@@ -23,6 +47,15 @@ function displayFuzzySearch()
 	let searchTerm = url.substring(url.lastIndexOf("/") + 1);
 	if(searchTerm == "404.html") return;
 
-	console.log(searchTerm);
-	//todo
+	let [found, correction] = fuzzySearch(searchTerm, PAGES);
+	if(found)
+	{
+		let msg = document.getElementById("msg");
+		msg.innerHTML = "Did you mean to go to <a href=\"josh8.com/"
+			+ correction + "\">/"
+			+ correction + "</a>?";
+
+		console.log("correction: " + correction);
+	}
+	else console.log("correction for " + searchTerm + " not found");
 }
