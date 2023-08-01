9c8a18bbfeaf9292514da92083cb0b7caa0cf7d8
joshnatis <joshnatis0@gmail.com>
2023-08-01 02:30:42 -0400

    (albums): create /albums page, deprecate /tunes/albums

---
+++ albums/green_line/tracklist.js
@@ -0,0 +1,39 @@
+albums['Green Line'] = {
+	'artist': 'Sealed Train',
+	'date': 'March 14th, 2023',
+	'video': 'https://www.youtube.com/watch?v=hll2ul0D_2k',
+};
+
+albums['Green Line']['tracklist'] =
+[
+	{ "name": "storage unit talisman",
+	  "time": "00:00 - 00:44",
+	},
+	{ "name": "sun showers",
+	  "time": "00:45 - 01:43",
+	},
+	{ "name": "sun kisss",
+	  "time": "01:44 - 02:27",
+	},
+	{ "name": "I walk you",
+	  "time": "02:28 - 03:50",
+	},
+	{ "name": "escape",
+	  "time": "03:51 - 04:34",
+	},
+	{ "name": "song of falling leaves",
+	  "time": "04:35 - 05:36",
+	},
+	{ "name": "fuji nights",
+	  "time": "05:37 - 08:55",
+	},
+	{ "name": "song of shrubs",
+	  "time": "08:56 - 10:51",
+	},
+	{ "name": "dust bunnies",
+	  "time": "10:52 - 11:14",
+	},
+	{ "name": "harping on piano",
+	  "time": "11:15 - 13:16",
+	}
+];
---
+++ albums/twin_peaks/tracklist.js
@@ -0,0 +1,29 @@
+albums['twin peaks'] = {
+	'artist': 'Punkt Yasha',
+	'date': 'July 7th, 2022'
+};
+
+albums['twin peaks']['tracklist'] =
+[
+	{ "name": "A001",
+	  "time": "00:00 - 00:41",
+	},
+	{ "name": "A002",
+	  "time": "00:42 - 01:36",
+	},
+	{ "name": "A003",
+	  "time": "01:37 - 02:20",
+	},
+	{ "name": "A004",
+	  "time": "02:21 - 02:52",
+	},
+	{ "name": "A005",
+	  "time": "02:53 - 04:02"
+	},
+	{ "name": "A006",
+	  "time": "04:03 - 04:17",
+	},
+	{ "name": "A007",
+	  "time": "04:18 - 05:53",
+	},
+];
