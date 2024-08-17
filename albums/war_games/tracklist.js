0e72ed86ad3a64114366a74766d95ea73a04ec5f
joshnatis <joshnatis0@gmail.com>
2024-08-17 14:45:56 -0400

    (albums): upload war games, snow01, intoxicated

---
+++ albums/war_games/tracklist.js
@@ -0,0 +1,33 @@
+albums['War Games'] = {
+	'artist': 'Sealed Train',
+	'date': 'April 11th, 2024',
+	'video': 'https://www.youtube.com/watch?v=oPFiwj1gmSM',
+};
+
+albums['War Games']['tracklist'] =
+[
+	{ "name": "A001",
+	  "time": "00:00 - 00:37",
+	},
+	{ "name": "A002",
+	  "time": "00:38 - 01:20",
+	},
+	{ "name": "A003",
+	  "time": "01:21 - 02:05",
+	},
+	{ "name": "A004",
+	  "time": "02:06 - 02:43",
+	},
+	{ "name": "A005",
+	  "time": "02:56 - 03:21",
+	},
+	{ "name": "A006",
+	  "time": "03:22 - 06:33",
+	},
+	{ "name": "A007",
+	  "time": "06:34 - 08:19",
+	},
+	{ "name": "A008",
+	  "time": "08:20 - 10:14",
+	}
+];
