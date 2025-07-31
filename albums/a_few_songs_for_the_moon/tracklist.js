e48b67818b4bd72bbe90b9c3cf626a8bbc234a3a
joshnatis <joshnatis0@gmail.com>
2025-07-31 18:38:39 -0400

    (albums): upload a few songs for the moon

---
+++ albums/a_few_songs_for_the_moon/tracklist.js
@@ -0,0 +1,20 @@
+albums['A Few Songs for the Moon'] = {
+	'artist': 'Sealed Train',
+	'date': 'July 2nd, 2025',
+};
+
+albums['A Few Songs for the Moon']['tracklist'] =
+[
+	{ "name": "Big gun",
+	  "time": "00:00 - 03:44",
+	},
+	{ "name": "Umma",
+	  "time": "03:45 - 07:08",
+	},
+	{ "name": "Endocrine disruptor",
+	  "time": "07:09 - 10:49",
+	},
+	{ "name": "Very low star power",
+	  "time": "10:50 - 16:04",
+	},
+];
